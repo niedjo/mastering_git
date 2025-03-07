0e6e45463b6916befedeeaba3484080b2cecd3d6
niedjo <niedjokuitche@gmail.com>
2025-03-07 16:14:03 +0100

    save the day

--- hello.js
+++ hello.js
@@ -3,3 +3,4 @@ console.log("hi, I'm nkds and I've modified your codebase")
 console.log("hello github !")
 console.log('hello from dev-niedjo')
 console.log('TRYING OUT REVERT')
+// FIX THE BUG OF THE LATEST COMMIT
